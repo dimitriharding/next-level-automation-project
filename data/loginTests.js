d19ac6411eddac2a0d07164415d22c0854163f20
Dimitri Harding <vaughnharding@hotmail.com>
2021-06-24 11:52:49 -0500

    📃

---
+++ data/loginTests.js
@@ -0,0 +1,8 @@
+module.exports = [
+    {
+        title: 'Verify that the user can sign-in successfully',
+        email: 'user@email.com',
+        password: '12340',
+        expectedResults: '', 
+    }
+]
